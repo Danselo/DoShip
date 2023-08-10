061bf14ba0a87d1318db97287b5c3822e1d90023
Danselo <danselo185@gmail.com>
2023-08-09 22:10:10 -0500

    Add component product detail and styles with tailwind

---
+++ src/Components/ProductDetail/index.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+
+const ProductDetail = () => {
+    return(
+        <aside className="flex flex-col fixed bg-white right-0 border border-black rounded-lg w-[360px] h-[calc(100vh-68px)] top-68">
+            <div className='flex justify-between items-center p-6'>
+                <h2 className='font-medium text-xl'>Detail</h2>
+                <div>x</div>
+            </div>
+        </aside>
+    )
+}
+export default ProductDetail
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react"
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
+import ProductDetail from '../../Components/ProductDetail'
+
 function Home() {
     const [items,setItems] = useState(null); 
 
@@ -13,7 +15,6 @@ function Home() {
     return (
       <Layout>
         HOME
-        {
          <div className="grid grid-cols-4 gap-4 w-full max-w-screen-lg">
           {
             items?.map(item => (
@@ -21,7 +22,7 @@ function Home() {
             ))
           }
          </div>
-        }
+         <ProductDetail />
       </Layout>
     )
   }
