ee95be37a1ab411c5808daca2b77b0e5287724fa
Danselo <danselo185@gmail.com>
2023-10-05 09:26:43 -0500

    is added mobile responsive in product detail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -6,17 +6,17 @@ const ProductDetail = () => {
     const context = useContext(ShoppingCartContext)
     return(
         <aside 
-        className ={`${context.isOpenProductDetail ? 'flex' : 'hidden'}  flex-col fixed bg-white right-0 border   border-black rounded-lg w-[360px] h-[calc(100vh-68px)] top-68`}>
+        className ={`${context.isOpenProductDetail ? 'flex' : 'hidden'}  flex-col fixed bg-white right-0 border sm:w-full sm:h-[25rem]  border-black rounded-lg w-[360px] h-[calc(100vh-68px)] top-68`}>
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>Detail</h2>
                 <div>
                     <XMarkIcon className='h-6 cursor-pointer hover:text-orange-600' onClick={context.closeProductDetail} />
                 </div>
             </div>
-            <div className='overflow-y-scroll flex flex-col'>
-            <figure className='px-6'>
+            <div className='overflow-y-scroll flex flex-col sm:items-center'>
+            <figure className='px-6 '>
                 <img
-                className='w-full h-full rounded-lg'
+                className='w-full h-full sm:w-[200px] sm:[200px] rounded-lg'
                 src={context.showProductDetail.images?.[0]} 
                 alt={context.showProductDetail.title} />
             </figure>
